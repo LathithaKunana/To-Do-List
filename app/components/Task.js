a0bfb3bba4a369276507e26ab4b240d6343d3ab8
LathithaKunana <lathitha.kunana@younglings.africa>
2023-08-01 14:33:39 +0200

    added app folder

---
+++ app/components/Task.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Task = () => {
+    
+    return (
+        <View>
+            <Text>Hello</Text>
+        </View>
+    )
+}
+export default Task
+
+const styles = StyleSheet.create({
+    
+})
+

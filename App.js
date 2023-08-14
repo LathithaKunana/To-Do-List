a03d6cc7e165a05143a4b0c45e0b18a2c0dcad77
LathithaKunana <lathitha.kunana@younglings.africa>
2023-08-14 08:21:51 +0200

    added task js

--- App.js
+++ App.js
@@ -1,41 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import Task from './app/components/task';
+import { KeyboardAvoidingView, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import Task from './components/Task';
 
 
 
 export default function App() {
+  const [task, setTask] = useState();
+
   return (
     <View style={styles.container}>
-      <SafeAreaView style={styles.taskContainer}>
+      <SafeAreaView>
         {/*heading box and heading text */}
-          <View style={styles.inputBox}>
-            <Text style={styles.heading}> To Do List</Text>
-          </View>
+        <View style={styles.headerBox}>
+          <Text style={styles.heading}> To Do List</Text>
+        </View>
 
           {/*outputs */}
-          <View style={styles.input}>
-              <Text></Text>
-          </View>
-          <View style={styles.input}>
-              
-          </View>
-
-          {/*to do inputs */}
-          <View style={styles.toDoInput}>
-            
-              
-          </View>
-          <View style={styles.addIcon}>
-              
-          </View>
-
-
+        <View style={styles.myItems}> 
+          <Task text={'Task 1'} />
+          <Task text={'Task 2'} />
+        </View>   
         
 
+          {/*to do inputs */}
+        <KeyboardAvoidingView
+            style={styles.writeTaskBox}>
+
+          <TextInput style={styles.textInput} placeholder={'write a task'} />
+
+          <TouchableOpacity>
+            <View style={styles.addBox} >
+              <Text style={styles.addText}>+</Text>
+            </View>
+          </TouchableOpacity>
+
+        </KeyboardAvoidingView>
+
       </SafeAreaView>
-      
     </View>
   );
 }
@@ -64,34 +66,49 @@ const styles = StyleSheet.create({
     borderRadius : 4,
     alignSelf: 'center',
   },
-  inputBox : {
+  headerBox : {
     backgroundColor :"black",
     width: '90%',
     height: 70,
     padding : 0,
-    top: "10%",
+    top: "5%",
     borderRadius:  20,
     alignItems: 'center',
     justifyContent: 'center',
     alignSelf: 'center'
   },
-
-  toDoInput : {
-    width: '70%',
-    height: 45,
-    backgroundColor: 'whitesmoke',
-    borderRadius:  20,
-    margin: 28,
-    top: 420
+  
+  myItems: {
+    marginTop: 60
   },
 
-  addIcon : {
-    width:45,
-    height: 45,
-    top: 345,
-    left: 305,
-    borderRadius:  30,
-    backgroundColor: 'whitesmoke',
+  writeTaskBox: {
+    position: 'absolute',
+    top: 700,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
 
+  textInput: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: 'whitesmoke',
+    borderRadius: 60,
+    width: 250,
+    borderColor: 'grey',
+    borderWidth: 1
+  },
+
+  addBox : {
+    width: 60,
+    height: 60,
+    backgroundColor: 'whitesmoke',
+    borderRadius: 60,
+    borderColor: 'grey',
+    borderWidth: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 });
